b3830f46794f11d2d59d357eed663c5016f028b0
Matthew Davis <matthew@matthewdavis.io>
2020-12-05 21:12:08 -0500

    bump

--- test/main.tf
+++ test/main.tf
@@ -2,10 +2,10 @@ module "test" {
 
     source = "../"
 
-    name      = "ingress-controller"
-    namespace = "default"
-    aws_profile = "odin"
-    aws_region = "us-east-1"
-    cluster_name = "odin-devops-1"
+    name         = "ingress-controller"
+    namespace    = "default"
+    aws_profile  = "myawsprofilename"
+    aws_region   = "us-east-1"
+    cluster_name = "cluster-1"
 
 }
